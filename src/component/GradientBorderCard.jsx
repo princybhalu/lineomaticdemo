7c3c48e134f801ef70ea7013558fd88bae38fe69
hexylon-dev <princybhalu11@gmail.com>
2024-12-20 14:35:25 +0530

    done 1

--- src/component/GradientBorderCard.jsx
+++ src/component/GradientBorderCard.jsx
@@ -5,16 +5,16 @@ import AudioReactiveParticles from './avatar'; // Ensure the path to avatar is c
 const GradientBorderCard = () => {
   return (
     <div className="w-screen h-screen flex">
-      <div className="w-full h-full flex flex-col justify-center items-center">
-        <div className="h-1/2 w-3/4">
+      <div className="w-full h-full flex flex-col justify-center items-center z-3 overflow-hidden">
+        <div className="h-1/2 w-3/4 z-3">
           <AudioReactiveParticles />
         </div>
       </div>
-      <div className="w-full h-full flex flex-col justify-center items-center p-12 border-l border-solid border-[#919191]">
+      <div className="w-full h-full flex flex-col justify-center items-center p-12 border-l border-solid border-[#919191] relative">
+        <div className="radial-blur-overlay" />
+        <div className="radial-blur-overlay-bottom-left" />
+        <div className="radial-blur-overlay-top-right" />
         <div className="gradient-border-card m-8">
-          <div className="radial-blur-overlay" />
-          <div className="radial-blur-overlay-bottom-left" />
-          <div className="radial-blur-overlay-top-right" />
           <div className="card-content" />
         </div>
       </div>
